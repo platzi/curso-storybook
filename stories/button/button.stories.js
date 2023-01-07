20f6b79d92a98afd7a15f707e99a8059d5b0c90e
Estefany Aguilar <estefanyaguilar@MacBook-Pro-de-Estefany.local>
2023-01-07 09:56:42 -0500

    Add: Clase 6

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -1,7 +1,29 @@
 import { createButton } from './button'
 
 export default {
-  title: 'Design System/Atoms/Button'
+  title: 'Design System/Atoms/Button',
+  argTypes: {
+    label: {
+      name: 'label',
+      control: {
+        type: 'text'
+      }
+    },
+    style: {
+      name: 'style',
+      options: ['filled', 'outlined'],
+      control: {
+        type: 'radio'
+      }
+    },
+    size: {
+      name: 'style',
+      options: ['small', 'medium', 'large'],
+      control: {
+        type: 'radio'
+      }
+    }
+  }
 }
 
 const Template = ({ label, ...args }) => {
